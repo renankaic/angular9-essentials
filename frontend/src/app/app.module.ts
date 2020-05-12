e595ea187b54e2c6f841241c4b82cb7dde9518f6
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-05-12 12:04:32 -0300

    Feat - Adding a structural directive example

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,7 +16,8 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { RedDirective } from './directives/red.directive'
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive'
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { RedDirective } from './directives/red.directive'
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    RedDirective
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
