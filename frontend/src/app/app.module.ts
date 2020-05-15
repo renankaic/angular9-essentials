4d51497962cbb1ab214cf6a0fa46e820fde0e763
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-05-15 19:51:15 -0300

    Feat - Creating Product Update component, assigning it in the Router and adding the routerLink in the product-read component table

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -34,7 +34,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort'
 
 import localePt from '@angular/common/locales/pt'
-import { registerLocaleData } from '@angular/common'
+import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(localePt)
 
